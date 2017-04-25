a16398e5758c75b0c560140c3e25d5aa6faf9707
Bernat <bernatmv@gmail.com>
2017-04-25 22:57:36 +0200

    fix: for material-ui

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
+import FontIcon from 'material-ui/FontIcon';
+import {white} from 'material-ui/styles/colors';
 import * as styles from './styles.css';
 
 // Needed for onTouchTap 
@@ -12,8 +13,10 @@ injectTapEventPlugin();
 
 ReactDOM.render(
     <MuiThemeProvider>
-        <RaisedButton label="Default" />
-        <div className={styles.body}>Hi!</div>
+        <div className={styles.body}>
+            <FontIcon className="material-icons" color={white}>home</FontIcon>
+            Hi!
+        </div>
     </MuiThemeProvider>,
     document.getElementById('root')
 );
