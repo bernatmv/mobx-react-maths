4a96e8c9dee22ffe6d0462c02abc0e7f185c9cbd
Bernat <bernatmv@gmail.com>
2017-05-03 17:19:16 +0200

    feat: added decorators and MobX

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import FontIcon from 'material-ui/FontIcon';
 import {white} from 'material-ui/styles/colors';
 import * as styles from './styles.css';
+import {observableTodoStore} from './store/todoStore';
 
 // Needed for onTouchTap 
 // http://stackoverflow.com/a/34015469/988941 
@@ -20,3 +21,9 @@ ReactDOM.render(
     </MuiThemeProvider>,
     document.getElementById('root')
 );
+
+observableTodoStore.addTodo("read MobX tutorial");
+observableTodoStore.addTodo("try MobX");
+observableTodoStore.todos[0].completed = true;
+observableTodoStore.todos[1].task = "try MobX in own project";
+observableTodoStore.todos[0].task = "grok MobX tutorial";
