e7b662c554039c2aec205575f08140747f07ebdf
Bernat <bernatmv@gmail.com>
2017-05-06 01:12:22 +0200

    feat: finished maths for advancements

--- src/store/mathStoreBottom.js
+++ src/store/mathStoreBottom.js
@@ -94,16 +94,35 @@ export default class MathStoreBottom {
                 // N retroceso R3
                     //...
             // 2 -> ...
+        let currentStep1, currentStep2, currentStep3;
         spins.forEach(spin => {
             for (let x = 0; x <= avances; x++) {
-                // roll back x positions for R1
-                // check positions
-                for (let y = 0; y < (avances - x); y++) {
-                    // roll back y positions for R2
-                    // check positions
-                    for (let z = 0; z < (avances - x - y); z++) {
-                        // roll back z positions for R3
-                        // check positions
+                currentStep1 = this.repeatRollBackReel(SystemConstants.Reel_1, spin, x);
+                if (x === 0 || !this.havePrize(currentStep1)) {
+                    for (let y = 0; y < (avances - x); y++) {
+                        currentStep2 = this.repeatRollBackReel(SystemConstants.Reel_2, currentStep1, y);
+                        if (y === 0 || !this.havePrize(currentStep2)) {
+                            for (let z = 0; z < (avances - x - y); z++) {
+                                currentStep3 = this.repeatRollBackReel(SystemConstants.Reel_3, currentStep2, z);
+                                if ((avances - x - y - z) === 0) {
+                                    if (this.havePrize(currentStep3)) {
+                                        if (this.isBeautiful(currentStep3)) {
+                                            approved.push(currentStep3);
+                                        } else {
+                                            discarded.push(currentStep3);
+                                        }
+                                    }
+                                } else {
+                                    if (this.havePrize(currentStep3)) {
+                                        // stop exploring this branch (break from loop)
+                                        break;
+                                    } else {
+                                        // do nothing, continue exploring this branch in the next iteration
+                                        continue;
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -117,7 +136,7 @@ export default class MathStoreBottom {
     }
 
     normalize(position) {
-        let size = this.config.reels[0].length;
+        let size = this.config.reels[SystemConstants.Reel_1].length;
         if (position < 0) {
             return this.normalize(size - position);
         } else {
@@ -125,11 +144,28 @@ export default class MathStoreBottom {
         }
     }
 
+    isBeautiful(spin) {
+        //TODO: check for ugly spins (jackpots on top or below, etc)
+        return true;
+    }
+
+    repeatRollBackReel(reel, spin, times) {
+        let currentStep = spin;
+        for (let i = 0; i < times; i++) {
+            currentStep = this.rollBackReel(reel, currentStep);
+        }
+        return currentStep;
+    }
+
     rollBackReel(reel, spin) {
         let posR1 = reel === SystemConstants.Reel_1 ? this.normalize(spin.positions[SystemConstants.Reel_1] - 1) : spin.positions[SystemConstants.Reel_1];
         let posR2 = reel === SystemConstants.Reel_2 ? this.normalize(spin.positions[SystemConstants.Reel_2] - 1) : spin.positions[SystemConstants.Reel_2];
         let posR3 = reel === SystemConstants.Reel_3 ? this.normalize(spin.positions[SystemConstants.Reel_3] - 1) : spin.positions[SystemConstants.Reel_3];
         return this.allSpins[this.getId(posR1, posR2, posR3)];
+    }
+
+    havePrize(spin) {
+        return !!spin.prize;
     }
 
     calculatePrize(figures) {
