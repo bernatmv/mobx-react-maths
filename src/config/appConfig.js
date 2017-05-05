37f3327adf6e92391318e41b58737d54a386a502
Bernat <bernatmv@gmail.com>
2017-05-05 14:36:47 +0200

    feat: added bottom maths configuration

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,18 +1,100 @@
+
+// COMMON
+
+const NA = 0;
+const CH = 1;
+const OR = 2;
+const PL = 3;
+const PE = 4;
+const ST = 5;
+const ME = 6;
+const G7 = 7;
+const R7 = 8;
+const B7 = 9;
+const SU = 10;
+const SH = 11;
+const QB = 12;
+
+// BOTTOM
+
 export const appConfigBottom = {
     reels: [
-        [1,2,3],
-        [1,2,3],
-        [1,2,3]
+        [
+            R7,
+            PE,
+            SU,
+            ME,
+            SH,
+            OR,
+            PL,
+            G7,
+            ME,
+            PL,
+            B7,
+            OR,
+            SU,
+            CH,
+            G7,
+            ST
+        ],
+        [
+            B7,
+            PL,
+            OR,
+            SH,
+            CH,
+            SH,
+            ST,
+            R7,
+            PE,
+            ME,
+            SU,
+            OR,
+            ST,
+            G7,
+            CH,
+            SH
+        ],
+        [
+            SH,
+            CH,
+            SU,
+            PE,
+            B7,
+            PL,
+            ST,
+            SH,
+            R7,
+            CH,
+            G7,
+            ME,
+            SU,
+            PE,
+            PL,
+            OR
+        ]
     ],
-    paytable: {
-        '1': [1],
-        '2': [5],
-        '3': [10]
-    },
+    paytable: [
+        [],
+        [2],
+        [4],
+        [8],
+        [12],
+        [16],
+        [20],
+        [50],
+        [100],
+        [500],
+        [],
+        [1,2,6],
+        []
+    ],
     wild: [],
-    minigame: [],
-    bonos: []
+    minigame: [SU],
+    bonos: [SH]
 };
+
+// TOP
 
 export const appConfigTop = {
     reels: [
